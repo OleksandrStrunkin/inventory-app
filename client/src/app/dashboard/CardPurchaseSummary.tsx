d6452796c6eabb6ed3f5025ac09b389ced4c56b4
OleksandrStrunkin <oleksandr.strunkin@gmail.com>
2025-01-16 11:58:12 +0200

    end purchase component

--- client/src/app/dashboard/CardPurchaseSummary.tsx
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -1,6 +1,7 @@
 import { useGetDashboardMetricsQuery } from "@/state/api";
 import { TrendingDown, TrendingUp } from "lucide-react";
 import numeral from "numeral";
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 const CardPurchaseSummary = () => {
   const { data, isLoading } = useGetDashboardMetricsQuery();
@@ -29,23 +30,61 @@ const CardPurchaseSummary = () => {
                     : "0"}
                 </p>
                 {lastDataPoint && (
-                <p
-                  className={`text-sm ${
-                    lastDataPoint.changePercentage! >= 0
-                      ? "text-green-500"
-                      : "text-red-500"
-                  } flex ml-3`}
-                >
-                  {lastDataPoint.changePercentage! >= 0 ? (
-                    <TrendingUp className="w-5 h-5 mr-1" />
-                  ) : (
-                    <TrendingDown className="w-5 h-5 mr-1" />
-                  )}
-                  {Math.abs(lastDataPoint.changePercentage!)}%
-                </p>
-              )}
+                  <p
+                    className={`text-sm ${
+                      lastDataPoint.changePercentage! >= 0
+                        ? "text-green-500"
+                        : "text-red-500"
+                    } flex ml-3`}
+                  >
+                    {lastDataPoint.changePercentage! >= 0 ? (
+                      <TrendingUp className="w-5 h-5 mr-1" />
+                    ) : (
+                      <TrendingDown className="w-5 h-5 mr-1" />
+                    )}
+                    {Math.abs(lastDataPoint.changePercentage!)}%
+                  </p>
+                )}
               </div>
             </div>
+            {/*Charts*/}
+            <ResponsiveContainer width="100%" height={350} className="px-7">
+              <AreaChart
+                data={purchaseData}
+                margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
+              >
+                <XAxis
+                  dataKey="date"
+                  tick={false}
+                  axisLine={false}
+                />
+                <YAxis
+                  tick={false}
+                  tickLine={false}
+                  axisLine={false}
+                />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString("en")}`,
+                  ]}
+                  labelFormatter={(label)=>{
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US",{
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric"
+                    })
+                  }}               
+                />
+                <Area
+                  type="linear"
+                  dataKey="totalPurchased"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                  dot={false}
+                />
+              </AreaChart>
+            </ResponsiveContainer>
           </div>
         </>
       )}
