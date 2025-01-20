040b44b9c4ecebf58bb4444c99f15941bb19cf94
OleksandrStrunkin <108118341+OleksandrStrunkin@users.noreply.github.com>
2025-01-20 16:28:38 +0200

    add pie diagram

--- client/src/app/dashboard/CardExpenseSummary.tsx
+++ client/src/app/dashboard/CardExpenseSummary.tsx
@@ -1,28 +1,32 @@
-import { ExpenseByCategorySummary, useGetDashboardMetricsQuery } from '@/state/api';
+import {
+  ExpenseByCategorySummary,
+  useGetDashboardMetricsQuery,
+} from "@/state/api";
 import React from "react";
-import { Pie, PieChart, ResponsiveContainer } from 'recharts';
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
-type ExpenseSums ={
+type ExpenseSums = {
   [category: string]: number;
-}
+};
 
 const colors = ["#00C49F", "#0088FE", "#FFBB28"];
 
 const CardExpenseSummary = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
 
-  const expenseByCategorySummary = dashboardMetrics?.expenseByCategorySummary || [];
+  const expenseByCategorySummary =
+    dashboardMetrics?.expenseByCategorySummary || [];
 
   const expenseSums = expenseByCategorySummary.reduce(
-    (acc: ExpenseSums, items: ExpenseByCategorySummary)=>{
+    (acc: ExpenseSums, items: ExpenseByCategorySummary) => {
       const category = items.category + " Expense";
-      const amount = parseInt(items.amount,10);
-      if(!acc[category]) acc[category] = 0;
+      const amount = parseInt(items.amount, 10);
+      if (!acc[category]) acc[category] = 0;
       acc[category] += amount;
       return acc;
     },
     {}
-  )
+  );
 
   const expenseCategories = Object.entries(expenseSums).map(
     ([name, value]) => ({
@@ -30,6 +34,13 @@ const CardExpenseSummary = () => {
       value,
     })
   );
+
+  const totalExpenses = expenseCategories.reduce(
+    (acc, category: { value: number }) => acc + category.value,
+    0
+  );
+
+  const formattedTotalExpenses = totalExpenses.toFixed(2);
   return (
     <div className="row-span-3 bg-white shadow-md rounded-2xl flex flex-col justify-between">
       {isLoading ? (
@@ -40,20 +51,39 @@ const CardExpenseSummary = () => {
             <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
               Expense Summary
             </h2>
-            <hr/>
+            <hr />
           </div>
           <div className="xl:flex justify-between pr-7">
             <div className="relative basis-3/5">
               <ResponsiveContainer width="100%" height={140}>
                 <PieChart>
-                  <Pie data={expenseCategories} innerRadius={50} outerRadius={60} fill="#8884d8" dataKey="value" nameKey="name" cx="50%" cy="50%">
-
+                  <Pie
+                    data={expenseCategories}
+                    innerRadius={50}
+                    outerRadius={60}
+                    fill="#8884d8"
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                  >
+                    {expenseCategories.map((entry, index) => (
+                      <Cell
+                        key={`cel-${index}`}
+                        fill={colors[index % colors.length]}
+                      />
+                    ))}
                   </Pie>
                 </PieChart>
               </ResponsiveContainer>
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center basis-2/5">
+                <span className="font-bold text-xl">
+                  ${formattedTotalExpenses}
+                </span>
+              </div>
             </div>
           </div>
-          </>
+        </>
       )}
     </div>
   );
